1f71e6cd51af9822dfd906081ac4f0c9df100d4d
gracedillingham <44414278+gracedillingham@users.noreply.github.com>
2020-04-29 00:25:46 -0500

    RR needs testing, but appears to work

--- roundRobin.cpp
+++ roundRobin.cpp
@@ -9,8 +9,13 @@ void roundRobin::runRR() {
     //need process list and matching burst/execution time
 
     //variables
-    int processTimer = 2; //switch processes every 50 ms
-    int burstTime[] = {5,3,8,9}; //burst times for processes in order
+    int processTimer = 3; //switch processes every 50 ms
+    int burstTime[] = {3,5,8,9}; //burst times for processes in order
+    int arrivalTime[4] = {0,0,0,0}; //initialize arrival time array values to zero
+    int timeElapsed = 0; //keep track of time that has elapsed
+    int counter = 0;
+    double average = 0;
+    int toAdd = 0;
     queue<int> toComplete;
 
     //add all values to queue
@@ -23,10 +28,45 @@ void roundRobin::runRR() {
 
     //execute processes in order for a limited amount of time
     //continue this time-scheduled loop until each process has finished
-
     //loop while queue is not empty
     //run for allotted time then rotate
+    while(!toComplete.empty()){
+        cout << "in while" << endl;
 
+        if(toComplete.front() <= processTimer){
+
+            cout << "in if" << endl;
+
+            timeElapsed += toComplete.front();
+
+            cout << "at " << toComplete.front() << " time " << timeElapsed << endl;
+
+            //execute for the remainder of the process' burst time
+            toComplete.pop();
+
+        }//end if
+        else{
+
+            cout << "in else" << endl;
+
+            //execute for the length of processTimer
+            //adjust burst time remaining
+            toComplete.front() -= processTimer;
+
+            timeElapsed += processTimer;
+
+        }//end else
+
+        //adjust counter
+        if(counter >= 3){
+            counter = 0;
+        }
+        else{
+            counter++;
+        }
+
+
+    }//end while
 
 
     cout << "end round robin" << endl;
